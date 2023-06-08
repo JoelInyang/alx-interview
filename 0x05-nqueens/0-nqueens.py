573f81313b8192178ddb80d1f8178955aa91ccf2
JoelInyang <joelinyang2003@gmail.com>
2023-06-09 00:36:04 +0100

    alx-interview

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,34 +1,93 @@
-#!/usr/bin/env python3
-""" r, print N must be a number,
-        followed by a new line, and exit with the status 1
-        If N is smaller than 4, print N must be at least 4,
-        followed by a new line, and exit with the status 1
-    The program should print every possible solution to the problem
-        One solution per line
-        You don’t have to print the solutions in a specific order
-    You are only allowed to import the sys module """
+#!/usr/bin/python3
+"""
+This N queens puzzle is the challenge of placing N non-attacking queens on an
+NxN chessboard. This is a program that solves the N queens problem.
+"""
 import sys
 
 
-def nqueens(n: int):
+def printSolution(board):
+    """This print the coordinates row and column for the position of
+       each N queen in the posible solution
+    Arg:
+       - board: matrix[n][n], list of list.
     """
-    backtracking
+    solution = []
+    for i in range(len(board)):
+        for j in range(len(board)):
+            if board[i][j] == 1:
+                solution.append([i, j])
+    print(solution)
+
+
+def isSafe(board, row, col, n):
+    """This is checking if a queen can be placed on board[row][col]
+    Arg:
+       - board: matrix[n][n], list of list
+       - row: position i to check
+       - col: position j to check
+       - n: number of queens to placed
+    Return: True or False
+       - True: if the queen coulb be placed, save place
+       - False: if there is not a save place
     """
-    matrix = [[0 for x in range(n)] for y in range(n)]
-    print(str(matrix))
+    for i in range(col):
+        if board[row][i] == 1:
+            return False
+    for i, j in zip(range(row, -1, -1),
+                    range(col, -1, -1)):
+        if board[i][j] == 1:
+            return False
+    for i, j in zip(range(row, n, 1),
+                    range(col, -1, -1)):
+        if board[i][j] == 1:
+            return False
+
+    return True
+
+
+def solveNQUtil(board, col, n):
+    """This function to solve the n queen problem using Backtracking
+       fins the posibles board to placed all the n queens on it
+       in a save places
+    Arg:
+       - board: list of list, of size board[n][n], n number of queens
+       - col: starts to check since column 0 until n to placed
+              one queebn per position
+       - n: number of queens to be placed
+    Return:
+       - True: if all the queens are placed on the board
+       - False: if a queen can not be placed
+    """
+    if col == n:
+        printSolution(board)
+        return True
+
+    counter = False
+    for i in range(n):
+        if isSafe(board, i, col, n):
+            board[i][col] = 1
+            counter = solveNQUtil(board, col + 1, n) or counter
+            board[i][col] = 0
+    return counter
 
 
 if __name__ == "__main__":
-    if len(sys.argv) > 2 or len(sys.argv) < 2:
+    """ This Take the argv from the command line:
+        - nqueens N, where N is the number of queens to be placed
+    """
+    if not len(sys.argv) == 2:
         print("Usage: nqueens N")
-        exit(1)
+        sys.exit(1)
 
-    if not sys.argv[1].isdigit():
+    if not (sys.argv[1]).isdigit():
         print("N must be a number")
-        exit(1)
+        sys.exit(1)
 
-    if int(sys.argv[1]) < 4:
+    n = int(sys.argv[1])
+    if n < 4:
         print("N must be at least 4")
-        exit(1)
+        sys.exit(1)
 
-    nqueens(int(sys.argv[1]))
+    board = [[0 for i in range(n)] for j in range(n)]
+    solveNQUtil(board, 0, n)
