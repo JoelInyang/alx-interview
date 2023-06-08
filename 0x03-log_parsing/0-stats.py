052636671d33edf46382d03de32762155e9995d1
JoelInyang <joelinyang2003@gmail.com>
2023-06-09 00:41:51 +0100

    alx-interview

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,36 +1,29 @@
 #!/usr/bin/python3
-"""the below, reads stdin(standard input) line by line and computes metrics:"""
+"""This script that reads stdin line by line and computes metrics"""
+
 
 import sys
 
-cache = {'200': 0, '301': 0, '400': 0, '401': 0,
-         '403': 0, '404': 0, '405': 0, '500': 0}
-total_size = 0
-counter = 0
+
+file_size = 0
+status_codes = {"200": 0, "301": 0, "400": 0, "401": 0,
+                "403": 0, "404": 0, "405": 0, "500": 0}
 
 try:
-    for line in sys.stdin:
-        line_list = line.split(" ")
-        if len(line_list) > 4:
-            code = line_list[-2]
-            size = int(line_list[-1])
-            if code in cache.keys():
-                cache[code] += 1
-            total_size += size
-            counter += 1
-
-        if counter == 10:
-            counter = 0
-            print('File size: {}'.format(total_size))
-            for key, value in sorted(cache.items()):
-                if value != 0:
-                    print('{}: {}'.format(key, value))
-
-except Exception as err:
-    pass
-
+    for i, line in enumerate(sys.stdin, 1):
+        splited = line.split(" ")
+        if len(splited) < 2:
+            continue
+        if splited[-2] in status_codes:
+            status_codes[splited[-2]] += 1
+        file_size += eval(splited[-1])
+        if i % 10 == 0:
+            print("File size: {}".format(file_size))
+            for key, value in sorted(status_codes.items()):
+                if value > 0:
+                    print("{}: {}".format(key, value))
 finally:
-    print('File size: {}'.format(total_size))
-    for key, value in sorted(cache.items()):
-        if value != 0:
-            print('{}: {}'.format(key, value))
+    print("File size: {}".format(file_size))
+    for key, value in sorted(status_codes.items()):
+        if value > 0:
+            print("{}: {}".format(key, value))
